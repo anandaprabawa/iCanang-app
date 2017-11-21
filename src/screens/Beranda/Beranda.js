c098cf654b5045d8da3952585e5c27036a8e0849
anandawp <ananda.widiprabawa@gmail.com>
2017-11-21 14:34:55 +0800

    update

--- src/screens/Beranda/Beranda.js
+++ src/screens/Beranda/Beranda.js
@@ -93,6 +93,10 @@ export default class Beranda extends React.PureComponent {
     this.refs.drawer.closeDrawer();
   }
 
+  formatToCurrency(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+  }
+
   renderHeader() {
     return (
       <View style={styles.viewBackground}>
@@ -139,7 +143,9 @@ export default class Beranda extends React.PureComponent {
         <Text numberOfLines={2} style={styles.cardTitle}>
           {item.nama}
         </Text>
-        <Text style={styles.cardHarga}>Rp {item.harga}</Text>
+        <Text style={styles.cardHarga}>
+          Rp {this.formatToCurrency(item.harga)}
+        </Text>
         <View style={styles.cardLocation}>
           <Icon android="md-pin" ios="ios-pin" style={styles.cardLocationPin} />
           <Text style={styles.cardLocationText}>{item.lokasi}</Text>
